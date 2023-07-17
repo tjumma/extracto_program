da17fefecffd844849f6e711df548ca98e878acd
tjumma <tim@jumma.pro>
2023-07-18 01:20:16 +0300

    Proper clockwork counter thread

--- programs/extracto_program/src/lib.rs
+++ programs/extracto_program/src/lib.rs
@@ -1,13 +1,14 @@
 use anchor_lang::prelude::*;
-// use anchor_lang::solana_program::{
-//     instruction::Instruction, native_token::LAMPORTS_PER_SOL, system_program,
-// };
-// use anchor_lang::InstructionData;
-// use clockwork_sdk::state::{Thread, ThreadAccount};
+use anchor_lang::solana_program::{
+    instruction::Instruction, native_token::LAMPORTS_PER_SOL, system_program,
+};
+use anchor_lang::InstructionData;
+use clockwork_sdk::state::Thread;
 
 declare_id!("CHPyHid6CQzErEYrsuinBRsjPdsUZdUzgKMCc6VZ9Tjf");
 
 pub const COUNTER_SEED: &[u8] = b"counter_1";
+pub const THREAD_AUTHORITY_SEED: &[u8] = b"thread_authority";
 
 #[program]
 pub mod extracto_program {
@@ -17,6 +18,63 @@ pub mod extracto_program {
         let counter = &mut ctx.accounts.counter;
         counter.count = 0;
         msg!("Counter account created. Current count: {}", counter.count);
+        Ok(())
+    }
+
+    pub fn start_thread(ctx: Context<StartThread>, thread_id: Vec<u8>) -> Result<()> {
+        // Get accounts.
+        let system_program = &ctx.accounts.system_program;
+        let clockwork_program = &ctx.accounts.clockwork_program;
+        let payer = &ctx.accounts.payer;
+        let thread = &ctx.accounts.thread;
+        let thread_authority = &ctx.accounts.thread_authority;
+        let counter = &mut ctx.accounts.counter;
+
+        // 1️⃣ Prepare an instruction to be automated.
+        let target_ix = Instruction {
+            program_id: ID,
+            accounts: crate::accounts::IncrementViaThread {
+                counter: counter.key(),
+                thread: thread.key(),
+                thread_authority: thread_authority.key(),
+            }
+            .to_account_metas(Some(true)),
+            data: crate::instruction::IncrementViaThread {}.data(),
+        };
+
+        // 2️⃣ Define a trigger for the thread (every 10 secs).
+        let trigger = clockwork_sdk::state::Trigger::Cron {
+            schedule: "*/10 * * * * * *".into(),
+            skippable: true,
+        };
+
+        // 3️⃣ Create thread via CPI.
+        let bump = *ctx.bumps.get("thread_authority").unwrap();
+        clockwork_sdk::cpi::thread_create(
+            CpiContext::new_with_signer(
+                clockwork_program.to_account_info(),
+                clockwork_sdk::cpi::ThreadCreate {
+                    payer: payer.to_account_info(),
+                    system_program: system_program.to_account_info(),
+                    thread: thread.to_account_info(),
+                    authority: thread_authority.to_account_info(),
+                },
+                &[&[THREAD_AUTHORITY_SEED, &[bump]]],
+            ),
+            LAMPORTS_PER_SOL,       // amount
+            thread_id,              // id
+            vec![target_ix.into()], // instructions
+            trigger,                // trigger
+        )?;
+
+        Ok(())
+    }
+
+    pub fn increment_via_thread(ctx: Context<IncrementViaThread>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented. Current count: {}", counter.count);
         Ok(())
     }
 
@@ -39,7 +97,7 @@ pub mod extracto_program {
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(
-        init, 
+        init,
         payer = user,
         seeds = [COUNTER_SEED],
         bump,
@@ -51,9 +109,54 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Increment<'info> {
+#[instruction(thread_id: Vec<u8>)]
+pub struct StartThread<'info> {
     #[account(mut)]
     pub counter: Account<'info, Counter>,
+
+    /// The Clockwork thread program.
+    #[account(address = clockwork_sdk::ID)]
+    pub clockwork_program: Program<'info, clockwork_sdk::ThreadProgram>,
+
+    /// The signer who will pay to initialize the program.
+    /// (not to be confused with the thread executions).
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    /// The Solana system program.
+    #[account(address = system_program::ID)]
+    pub system_program: Program<'info, System>,
+
+    /// Address to assign to the newly created thread.
+    #[account(mut, address = Thread::pubkey(thread_authority.key(), thread_id))]
+    pub thread: SystemAccount<'info>,
+
+    /// The pda that will own and manage the thread.
+    #[account(seeds = [THREAD_AUTHORITY_SEED], bump)]
+    pub thread_authority: SystemAccount<'info>,
+}
+
+#[derive(Accounts)]
+pub struct IncrementViaThread<'info> {
+    #[account(mut, seeds= [COUNTER_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+
+    /// Verify that only this thread can execute the Increment Instruction
+    #[account(signer, constraint = thread.authority.eq(&thread_authority.key()))]
+    pub thread: Account<'info, Thread>,
+
+    /// The Thread Admin
+    /// The authority that was used as a seed to derive the thread address
+    /// `thread_authority` should equal `thread.thread_authority`
+    #[account(seeds = [THREAD_AUTHORITY_SEED], bump)]
+    pub thread_authority: SystemAccount<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut, seeds= [COUNTER_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+
     pub user: Signer<'info>,
 }
 
